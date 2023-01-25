a302b84bae5ec5d2f3cc2babaae2a38e7953b2c4
JurTeuwen <jurteuwen@gmail.com>
2023-01-25 10:42:58 +0100

    new

--- app.py
+++ app.py
@@ -12,7 +12,14 @@ venster = tk.Tk()
 venster.title("vertaalApp")
 venster.geometry("500x300")
 
-
+msg = tk.Message(master=venster, text = "", width=300)
+msg.grid(row=0, column=0)
+msg = tk.Message(master=venster, text = "", width=300)
+msg.grid(row=1, column=0)
+msg = tk.Message(master=venster, text = "", width=300)
+msg.grid(row=2, column=0)
+msg = tk.Message(master=venster, text = "", width=300)
+msg.grid(row=3, column=0)
 
 def record2():
     global record
@@ -36,33 +43,54 @@ def spraakvertaling():
     print(tekst)
 
 def bestandvertaling():
+    knop4 = tk.Button(master=venster, text="vertaling", command=vertaling, width=70, height=2)
+    knop4.grid(row= 10, column=0)
+    msg = tk.Message(master=venster, text = "bestandvertaling", width=300)
+    msg.grid(row=0, column=0)
     import subprocess
     global tekst
     tk.Tk().withdraw()
     file_path = filedialog.askopenfilename(filetypes=(("Audio Files", ".wav .ogg .mp3 .aac .m4a"),   ("All Files", "*.*")))
     file_name = os.path.split(file_path)[1]
+    msg = tk.Message(master=venster, text = "bestand omzetten naar tekt", width=300)
+    msg.grid(row=1, column=0)
+    venster.update()
     subprocess = subprocess.Popen(f"py transcribe.py {file_name}", shell=True, stdout=subprocess.PIPE)
     tekst = subprocess.stdout.read()
+    msg = tk.Message(master=venster, text = "omzetten naar tekst is gelukt", width=300)
+    msg.grid(row=1, column=0)
     print(tekst)
+    tk.messagebox.showwarning("bestand",tekst)
 
 vlak = ""
 def tekstvertaling():
+    knop4 = tk.Button(master=venster, text="vertaling", command=vertaling, width=70, height=2)
+    knop4.grid(row= 10, column=0)
+    msg = tk.Message(master=venster, text = "", width=300)
+    msg.grid(row=0, column=0)
+    msg = tk.Message(master=venster, text = "tekstvertaling", width=300)
+    msg.grid(row=0, column=0)
+    msg = tk.Message(master=venster, text = "", width=300)
+    msg.grid(row=1, column=0)
     global vlak
     global tekst
     button = tk.Button(master=venster, text="bevestigen", command=bevestigen)
-    button.grid(row=8, column=0)
+    button.grid(row=2, column=0)
     vlak = tk.Entry()
-    vlak.grid(row=4, column=0)
+    vlak.grid(row=3, column=0)
     
 
 def bevestigen():
+    knop4 = tk.Button(master=venster, text="vertaling", command=vertaling, width=70, height=2)
+    knop4.grid(row= 10, column=0)
     global tekst
     tekst=vlak.get()
 
 def vertaling():
-    msg = tk.Message(master=venster, text = "bezig met vertalen")
-    msg.grid(row=0, column=0)
-    venster.update()
+    knop4 = tk.Button(master=venster, text="vertaling", command=vertaling, width=70, height=2)
+    knop4.grid(row= 10, column=0)
+    msg = tk.Message(master=venster, text = "bezig met vertalen", width=300)
+    msg.grid(row=1, column=0)
     global tekst
     url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
 
@@ -76,17 +104,16 @@ def vertaling():
 
     response = requests.request("POST", url, data=payload, headers=headers)
     print(response.json())
-    msg = tk.Message(master=venster, text = "vertaling is gelukt")
-    msg.grid(row=2, column=1)
+    msg = tk.Message(master=venster, text = "vertaling is gelukt", width=300)
+    msg.grid(row=1, column=0)
     text = response.json()
     tk.messagebox.showwarning("vertaling",text["data"]["translations"][0]["translatedText"])
-    msg = tk.Message(master=venster, text = "")
-    msg.grid(row=2, column=1)
+    msg = tk.Message(master=venster, text = "", width=300)
+    msg.grid(row=1, column=0)
 
 
 label = tk.Label(master=venster, text="vetaalapp")
-knop4 = tk.Button(master=venster, text="vertaling", command=vertaling, width=70, height=2)
-knop4.grid(row= 10, column=0)
+
 
 menubar = tk.Menu(venster)  
 file1 = tk.Menu(menubar, tearoff=0)  
