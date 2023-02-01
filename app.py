a71f6d50e35df6b2835884161045250f84ac30be
Vami220805 <90028381+Vami220805@users.noreply.github.com>
2023-02-01 16:19:59 +0100

    finished speech/file convert to text

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # het invoegen van de library's die we gebruiken
 import tkinter as tk
 from tkinter import filedialog
+import queue
+import soundfile as sf
+import sounddevice as sd
+import threading
 import requests
 import os
+
 
 # aanmaken van widgets die we gebruiken
 tekst=""
@@ -25,6 +30,12 @@ labelHome3=""
 venster = tk.Tk()
 venster.title("vertaalApp")
 venster.geometry("500x300")
+
+#Create a queue to contain the audio data
+q = queue.Queue()
+#Declare variables and initialise them
+recording = False
+file_exists = False
 
 
 # de code die we gebruiken als je kiest voor spraakvertaling
@@ -89,11 +100,71 @@ def spraakvertaling():
     except AttributeError:
         pass
     venster.update()    # update het venster
-    # dit is de code die we gebruiken voor spraakvertaling, deels op internet gevonden en deels zelf gemaakt
-    knop = tk.Button(master=venster, text="vertaling", command=vertaling, font=("Arial", 15))
-    knop.place(relx=0.5, rely=0.85, anchor="center")
-    msgSV = tk.Message(master=venster, text = "spraakvertaling", width=300, font=("Arial", 20))
-    msgSV.place(relx=0.5, rely=0.05, anchor="center")
+    #Button to record audio
+    record_btn = tk.Button(venster, text="Start opname", command=lambda m=1:threading_rec(m))
+    #Stop button
+    stop_btn = tk.Button(venster, text="Stop opname", command=lambda m=2:threading_rec(m))
+    #Play button
+    play_btn = tk.Button(venster, text="Zet om naar tekst", command=lambda m=3:threading_rec(m))
+    #Position buttons
+    record_btn.grid(row=1,column=1)
+    stop_btn.grid(row=1,column=0)
+    play_btn.grid(row=1,column=2)
+    venster.mainloop()
+    spraak()
+
+#Fit data into queue
+def callback(indata, frames, time, status):
+    q.put(indata.copy())
+
+
+#Functions to play, stop and record audio in Python voice recorder
+#The recording is done as a thread to prevent it being the main process
+def threading_rec(x):
+   if x == 1:
+       #If recording is selected, then the thread is activated
+       t1=threading.Thread(target=spraak)
+       t1.start()
+   elif x == 2:
+       #To stop, set the flag to false
+       global recording
+       recording = False
+       tk.messagebox.showinfo(message="Opname gestopt.")
+   elif x == 3:
+       #To play a recording, it must exist.
+       if file_exists:
+            import subprocess
+            #Read the recording if it exists and play it
+            # data, fs = sf.read("trial.wav", dtype='float32')
+            subprocess = subprocess.Popen(f"py transcribe.py trial.wav", shell=True, stdout=subprocess.PIPE)
+            tekst = subprocess.stdout.read()
+            msg2 = tk.Message(master=venster, text = "omzetten naar tekst is gelukt", width=300, font=("Arial", 12))
+            msg2.place(relx=0.5, rely=0.40, anchor="center")
+            tk.messagebox.showwarning("Vertaalde tekst",tekst)
+            knop = tk.Button(master=venster, text="Vertaal gevonden tekst van EN naar NL", command=vertaling, font=("Arial", 15))
+            knop.place(relx=0.5, rely=0.85, anchor="center")
+       else:
+           #Display and error if none is found
+           tk.messagebox.showerror(message="Geen opname gevonden, probeer opnieuw...")
+
+
+def spraak():
+    #Declare global variables   
+    global recording
+    #Set to True to record
+    recording= True  
+    global file_exists
+    #Create a file to save the audio
+    tk.messagebox.showinfo(message="Begin met opnemen...")
+    with sf.SoundFile("trial.wav", mode='w', samplerate=44100,
+                        channels=2) as file:
+    #Create an input stream to record audio without a preset time
+            with sd.InputStream(samplerate=44100, channels=2, callback=callback):
+                while recording == True:
+                    #Set the variable to True to allow playing the audio later
+                    file_exists =True
+                    #write into file
+                    file.write(q.get())
 
 
 # de code die we gebruiken als je kiest voor bestandvertaling
@@ -161,20 +232,23 @@ def bestandvertaling():
     # dit is de code die we gebruiken voor bestandvertaling, deels op internet gevonden en deels zelf gemaakt
     # bij deze code mken we ook gebruik van de bestanden transcribe.py en utils.py
     import subprocess
-    knop = tk.Button(master=venster, text="vertaling", command=vertaling, font=("Arial", 15))
-    knop.place(relx=0.5, rely=0.85, anchor="center")
-    msgBV = tk.Message(master=venster, text = "bestandvertaling", width=300, font=("Arial", 20))
+    msgBV = tk.Message(master=venster, text = "Bestandvertaling", width=300, font=("Arial", 20))
     msgBV.place(relx=0.5, rely=0.05, anchor="center")
     file_path = filedialog.askopenfilename(filetypes=(("Audio Files", ".wav .ogg .mp3 .aac .m4a"),   ("All Files", "*.*")))
     file_name = os.path.split(file_path)[1]
-    msg1 = tk.Message(master=venster, text = "bestand omzetten naar tekt", width=300, font=("Arial", 12))
+    msg1 = tk.Message(master=venster, text = "Bestand omzetten naar tekst, dit kan even duren...", width=300, font=("Arial", 12))
     msg1.place(relx=0.5, rely=0.40, anchor="center")
     venster.update()
-    subprocess = subprocess.Popen(f"py transcribe.py {file_name}", shell=True, stdout=subprocess.PIPE)
-    tekst = subprocess.stdout.read()
-    msg2 = tk.Message(master=venster, text = "omzetten naar tekst is gelukt", width=300, font=("Arial", 12))
-    msg2.place(relx=0.5, rely=0.40, anchor="center")
-    tk.messagebox.showwarning("bestand",tekst)
+    if file_name != "":
+        subprocess = subprocess.Popen(f"py transcribe.py {file_name}", shell=True, stdout=subprocess.PIPE)
+        tekst = subprocess.stdout.read()
+        msg2 = tk.Message(master=venster, text = "omzetten naar tekst is gelukt", width=300, font=("Arial", 12))
+        msg2.place(relx=0.5, rely=0.40, anchor="center")
+        tk.messagebox.showwarning("Vertaalde tekst",tekst)
+        knop = tk.Button(master=venster, text="Vertaal gevonden tekst", command=vertaling, font=("Arial", 15))
+        knop.place(relx=0.5, rely=0.85, anchor="center")
+    else:
+        tk.messagebox.showwarning("Vertaalde tekst","Tekst niet vertaald, probeer alstublieft opnieuw.")
 
 
 # de code die we gebruiken als je kiest voor bestandvertaling
@@ -317,12 +391,12 @@ labelHome4.place(relx=1, rely=1, anchor="se")
 
 menubar = tk.Menu(venster)  
 vertaler = tk.Menu(menubar, tearoff=0)  
-vertaler.add_command(label="spraak", command=spraakvertaling)  
-vertaler.add_command(label="bestand", command=bestandvertaling)  
-vertaler.add_command(label="tekst", command=tekstvertaling)
+vertaler.add_command(label="Spraak", command=spraakvertaling)  
+vertaler.add_command(label="Bestand", command=bestandvertaling)  
+vertaler.add_command(label="Tekst vertaler", command=tekstvertaling)
 vertaler.add_separator()
 vertaler.add_command(label="Exit", command=venster.quit)  
-menubar.add_cascade(label="vertaler", menu=vertaler)
+menubar.add_cascade(label="Vertaler", menu=vertaler)
   
 venster.config(menu=menubar)
 venster.mainloop()
